075692f29603292ed90448228f81e71e349afe95
qhobo <qhobosheanebokhabane@gmail.com>
2025-12-09 08:33:59 +0200

    done adding frond end

--- API/Program.cs
+++ API/Program.cs
@@ -10,17 +10,16 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
 opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
